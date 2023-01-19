3fbc2faf9142eac948ea39b0d7fb95c003d6966b
ylmngo <aamerasim45@gmail.com>
2023-01-19 07:45:33 -0800

    Added QR functionalities

--- qr/urls.py
+++ qr/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('college/', views.c_view), 
     path('college/block/<int:bid>', views.b_view), 
     path('college/block/<int:bid>/floor/<int:fid>', views.f_view), 
+    path('college/block/<int:bid>/floor/act/<int:fid>', views.qr_view), 
 ]
--- qr/views.py
+++ qr/views.py
@@ -14,7 +14,13 @@ def c_view(request):
     return HttpResponse(template.render(context, request))
 
 def b_view(request, bid): 
-    pass 
+    template = loader.get_template('qr/c.html')
+    c = College.objects.get(id=0)
+    context  = {
+        'c': c, 
+        'img': c.aimg,
+    }
+    return HttpResponse(template.render(context, request))
 
 def f_view(request, bid, fid): 
     c = College.objects.get(id=0)
@@ -35,6 +41,32 @@ def f_view(request, bid, fid):
     context = {
         'f': f, 
         'img': f.img, 
+        'up' : up, 
+        'down': down, 
+        'fu' : fu,
+        'fd' : fd, 
+    } 
+    return HttpResponse(template.render(context, request)) 
+
+def qr_view(request, bid, fid): 
+    c = College.objects.get(id=0)
+    b = Block.objects.get(id=bid)
+    f = Floor.objects.get(id=fid)
+    up = False
+    down = False
+    fu = 0 
+    fd = 0  
+    if ((fid%10) + 1) <= b.num: 
+        up = True
+        fu = fid + 1   
+    if((fid%10) - 1)  > 0:
+        down = True 
+        fd   = fid - 1
+        print(down) 
+    template = loader.get_template('qr/qr.html')
+    context = {
+        'f': f, 
+        'img': f.aimg, 
         'up' : up, 
         'down': down, 
         'fu' : fu,
